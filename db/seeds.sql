0422507839937a4e697da567b4b47f4a3c9ac67c
csullins <camilesullins@gmail.com>
2023-05-06 11:42:22 -0500

    add db schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (id, name)
+VALUES  (001, "Executive"),
+        (002, "Engineering"),
+        (003, "Accounting"),
+        (004, "IT"),
+        (005, "Customer Service");
+       
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 300000, 001),
+       ("Software Engineer", 130000, 002),
+       ("Accountant", 80000, 003),
+       ("System Administrator", 90000, 004),
+       ("Customer Service Associate", 50000, 005);
+       
